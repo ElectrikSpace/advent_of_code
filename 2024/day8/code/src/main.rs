cbd5fb5e5e5595b837ecf329d38b6a84939b845d
Sylvain Noiry <sylvain.noiry@hotmail.fr>
2024-12-13 08:46:24 +0100

    Day 8 Part2

--- 2024/day8/code/src/main.rs
+++ 2024/day8/code/src/main.rs
@@ -2,25 +2,6 @@ use std::fs;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-//#[derive(Clone, Copy)]
-//enum Direction {
-//    Up = 0,
-//    Right = 1,
-//    Down = 2,
-//    Left = 3,
-//}
-//
-//impl Direction {
-//    fn turn_right(&mut self) {
-//        *self = match self {
-//            Direction::Up => Direction::Right,
-//            Direction::Right => Direction::Down,
-//            Direction::Down => Direction::Left,
-//            Direction::Left => Direction::Up,
-//        };
-//    }
-//}
-
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
     x: i32,
@@ -40,10 +21,14 @@ impl Grid {
         };
     }
 
-    fn insert(&mut self, p: Point) {
+    fn insert(&mut self, p: Point) -> bool {
         if (p.x < self.bound.x) && (p.y < self.bound.y) &&
            (p.x >= 0) && (p.y >= 0) {
             self.points.insert(p);
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
@@ -52,14 +37,12 @@ fn main() {
     let contents = fs::read_to_string("input")
         .expect("Should have been able to read the file");
 
+    // Init structures
     let bound = Point {
         x: contents.lines().next().unwrap().len() as i32,
         y: contents.lines().count() as i32
     };
     let mut antennas: HashMap<char, Grid> = HashMap::new();
-    let mut antinodes = Grid::new(bound);
-    
-    // Init structures
     let _ = contents.lines()
         .enumerate()
         .for_each(|(i, a)| {
@@ -81,6 +64,8 @@ fn main() {
         });
 
     // Compute antinodes
+    let mut antinodes = Grid::new(bound);
+    let mut antinodes2 = Grid::new(bound);
     let _ = antennas.values()
                 .for_each(|g| {
                     g.points
@@ -96,18 +81,33 @@ fn main() {
                                         y: t.y - p.y
                                     };
                                     // antinode 1
-                                    antinodes.insert(Point {
+                                    let mut a1 = Point {
                                         x: p.x - v.x,
                                         y: p.y - v.y
-                                    });
+                                    };
                                     // antinode 2
-                                    antinodes.insert(Point {
+                                    let mut a2 = Point {
                                         x: t.x + v.x,
                                         y: t.y + v.y
-                                    });
+                                    };
+                                    // part 1
+                                    antinodes.insert(a1);
+                                    antinodes.insert(a2);
+                                    // part 2
+                                    antinodes2.insert(*p);
+                                    antinodes2.insert(*t);
+                                    while antinodes2.insert(a1) {
+                                        a1.x = a1.x - v.x;
+                                        a1.y = a1.y - v.y;
+                                    }
+                                    while antinodes2.insert(a2) {
+                                        a2.x = a2.x + v.x;
+                                        a2.y = a2.y + v.y;
+                                    }
                                 })
                     })
                 });
-    let antinodes_count = antinodes.points.len();
-    println!("There are {antinodes_count} antinodes");
+
+    println!("Part1: There are {} antinodes", antinodes.points.len());
+    println!("Part2: There are {} antinodes", antinodes2.points.len());
 }
